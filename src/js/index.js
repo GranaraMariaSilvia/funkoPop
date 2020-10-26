2e0d58397bbc4ce4539612f550610485b866c748
Silvia Granara <masil79@gmail.com>
2020-10-25 21:37:08 -0300

    se agrego la logica de la pagina de inicio

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-import '../css/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "../css/style.css";
 
+leerStorage();
 
-console.log("hola mundo");
+function leerStorage() {
+  if (localStorage.length > 0) {
+    let listaProductos = JSON.parse(localStorage.getItem("funkoKey"));
+    let catalogo = document.getElementById("catalogo");
+
+    for (let i in listaProductos) {
+      let codigoHtml = `<div class="col-sm-12 col-md-4 col-lg-3">
+
+            <div class="card">
+              <img src="./img/productos/${listaProductos[i].imagen}" class="card-img-top" alt="${listaProductos[i].nombre}">
+              <div class="card-body">
+                <h5 class="card-title">${listaProductos[i].nombre}</h5>
+                <p class="card-text">${listaProductos[i].descripcion}.</p>
+                <a href="#" class="btn btn-secondary disabled">Comprar</a>
+              </div>
+            </div>
+  
+          </div> `;
+
+         catalogo.innerHTML += codigoHtml;
+    }
+  }
+}
